2c8a50e9b3e0aed4a64f42cbc4af9458fcabe9e2
Shivam <shivam.srivastava31093@gmail.com>
2017-05-30 01:32:59 +0530

    Initial commit

---
+++ src/intro/HelloWorld.kt
@@ -0,0 +1,15 @@
+package intro
+
+/**
+ * Created by sHIVAM on 5/30/2017.
+ */
+
+fun main(args: Array<String>) {
+    println(sayHello())
+}
+
+fun sayHello() = "Hello World!"
+
+fun sayHello1(): String {
+    return "Hello World";
+}
